4852f5a153c968660fc3622d88335d92eacccb92
Oleksandr Sych <sychalexandr@ukr.net>
2020-09-04 07:09:48 +0000

    test

--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesController } from './universities.controller';
 import { UniversitiesService } from './universities.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { DepartmentSchema } from './schemas/department.schema';
 
 @Module({
+  imports: [  
+    MongooseModule.forFeature([{ name: 'Department', schema: DepartmentSchema }]),],
   controllers: [UniversitiesController],
-  providers: [UniversitiesService]
+  providers: [UniversitiesService],
+  exports: [UniversitiesService],
 })
 export class UniversitiesModule {}
